c951670c01ecd5ad39d5ecd5e0254dd7c2a88b40
Saugandh <saugandhkaran@gmail.com>
2018-03-01 11:50:25 +0100

    Buttons added and css as well

--- app.js
+++ app.js
@@ -5,38 +5,55 @@ app.controller('testController',['$scope','$http', function($scope, $http){
 	$scope.conversationList = [];
 	$scope.timeStamps = [];
 
-	$scope.submit = function(){
+
+	function setQuestionObject(conversationObject, question){
+		conversationObject.options = [];
+		conversationObject.question = question;
 		var date = new Date();
+		conversationObject.timeStamp = date.toLocaleString();
+	}
+
+	function getResponse(conversation){
+		var data = {
+			"contexts": [
+			  ""
+			],
+			"lang": "en",
+			"query": " ",
+			"sessionId": "12345"
+		  };
+		data.query = conversation.question;
+		JSON.stringify(data);
+		$http.post('https://api.dialogflow.com/v1/query?v=20180228', data, {
+			headers : {'Authorization' : 'Bearer c4cc3fdb3bdb416cab5aef2737c6f9b1',
+						'Content-Type' : 'application/json'}
+					}
+				).then(function(success){
+					conversation.answer = success.data.result.fulfillment.messages[1].payload.result.speech;
+					conversation.options = success.data.result.fulfillment.messages[1].payload.result.options;
+				}, function(error){
+					alert(error);
+				}).then(function(){
+					$scope.conversationList.push(conversation);
+				})
+	}
+
+	$scope.submit = function(){
 		var conversation = {};
-		conversation.question = $scope.question;
-		conversation.timeStamp = date.toLocaleString();
+		setQuestionObject(conversation, $scope.question);
 		conversation.id = $scope.conversationList.length;
 		var question = $scope.question;
 		$scope.question = '';
+		getResponse(conversation);
+	}
 
-		var data = {
-				    "contexts": [
-				      ""
-				    ],
-				    "lang": "en",
-				    "query": " ",
-				    "sessionId": "12345",
-				    "timezone": "America/New_York"
-				  };
-		data.query = question;
-		JSON.stringify(data);
-		$http.post('https://api.dialogflow.com/v1/query', data, {
-			headers : {'Authorization' : 'Bearer 76b505a7cbcc43678fc08e5a5dbe2ce8',
-						'Content-Type' : 'application/json'}
-					}
-						).then(function(success){
-							conversation.answer = success.data.result.speech;
-						}, function(error){
-							alert(error);
-						}).then(function(){
-							$scope.conversationList.push(conversation);
-						})
+	$scope.selectedFromOption = function(optionSelected){
+		var conversation = {};
+		setQuestionObject(conversation, optionSelected);
+		optionSelected = undefined;
+		getResponse(conversation);
+		}
 	}
 
 
-}])
+])
